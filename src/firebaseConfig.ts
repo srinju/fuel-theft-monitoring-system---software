2d8aed21a41d7b9b02d0c6cf2b33d8cbc58b62e3
SRINJOY DAS <115316528+srinju@users.noreply.github.com>
2024-09-29 14:09:46 +0530

    Update firebaseConfig.ts

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getDatabase, ref, onValue, update, push , remove } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBK0m6em6fxGbtyVi6DZZV899T1nAMgk_0",
-  authDomain: "tanksafe-a61ae.firebaseapp.com",
-  databaseURL: "https://tanksafe-a61ae-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "tanksafe-a61ae",
-  storageBucket: "tanksafe-a61ae.appspot.com",
-  messagingSenderId: "193691636253",
-  appId: "1:193691636253:web:3c3e6fbb3443e2937f6cf4",
+  apiKey: "your api key",
+  authDomain: "your auth domain",
+  databaseURL: "your db url",
+  projectId: "your project id",
+  storageBucket: "your storage bucket string",
+  messagingSenderId: "your messaging sender id",
+  appId: "your app id",
 };
 
 const app = initializeApp(firebaseConfig);
